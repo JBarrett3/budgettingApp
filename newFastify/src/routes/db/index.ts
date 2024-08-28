74f8f6341dd4635948329eca614fd5603f1345e9
James Barrett <JamesRobertBarrett9@gmail.com>
2024-08-28 12:59:49 -0400

    working getter

--- newFastify/src/routes/db/index.ts
+++ newFastify/src/routes/db/index.ts
@@ -1,8 +1,17 @@
 import { FastifyPluginAsync } from "fastify"
 
 const dbRoutes: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/users', async function (request, reply) {
-    return fastify
+  fastify.get('/users', async function (req, res) {
+    res.send(await fastify.database.user.find({})).status(200)
+  })
+  fastify.get('/auths', async function (req, res) {
+    res.send(await fastify.database.auth.find({})).status(200)
+  })
+  fastify.get('/finances', async function (req, res) {
+    res.send(await fastify.database.finance.find({})).status(200)
+  })
+  fastify.get('/accounts', async function (req, res) {
+    res.send(await fastify.database.account.find({})).status(200)
   })
 }
 
